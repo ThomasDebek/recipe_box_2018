be2d086af7b8d92cb549b06083d7437c2a82a59a
tom.debek@gmail.com <tom.debek@gmail.com>
2019-01-10 23:13:13 +0100

    Add new structure gem cocoon

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,16 @@
 class Recipe < ApplicationRecord
+
+  has_many :ingredients
+  has_many :directions
+
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
+
+
+  validates :title, :description, :image, presence:true 
+
   has_attached_file :image, styles: { medium: "400x400>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-  has_many :directions
-  has_many :ingredients
+
 
 end
